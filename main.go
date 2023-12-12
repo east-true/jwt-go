88efdba444168bff6bb89f8357bcd3769e82dfcb
east-true <leik24@naver.com>
2023-12-13 01:08:35 +0900

    add token verify

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
@@ -9,12 +10,20 @@ import (
 var secret []byte = []byte("secret")
 
 type Claim struct {
-	Role string
+	Role string `json:"role"`
 	jwt.RegisteredClaims
 }
 
 func NewHmacToken() string {
-	claim := new(Claim)
+	claim := Claim{
+		RegisteredClaims: jwt.RegisteredClaims{
+			ID:        "test",
+			Subject:   "userid",
+			Issuer:    "boardsvr",
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 30)),
+		},
+	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claim)
 	signedToken, err := token.SignedString(secret)
 	if err != nil {
@@ -29,5 +38,23 @@ func main() {
 	token := NewHmacToken()
 	if token != "" {
 		fmt.Println(token)
+	}
+
+	claim := new(Claim)
+	jwtToken, err := jwt.ParseWithClaims(token, claim, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); ok {
+			return secret, nil
+		}
+
+		return nil, jwt.ErrInvalidType
+	})
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	if jwtToken.Valid {
+		fmt.Println(claim)
 	}
 }
