6f5ef91c3486cf97dd3eb8f80bf221f9600451b2
east-true <leik24@naver.com>
2024-02-22 16:10:20 +0900

    update claims

--- claims/claims.go
+++ claims/claims.go
@@ -1,68 +1,82 @@
 package claims
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	jwt "github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
 )
 
-var secret []byte = []byte("secret")
+var secret []byte
+
+func init() {
+	idgen, _ := uuid.NewRandom()
+	id := idgen.String()
+	secret = []byte(id)
+}
 
 type Claims struct {
 	Role string `json:"role"`
 	jwt.RegisteredClaims
 }
 
-func New(id, sub, iss, role string) *Claims {
+func New(uuid, role string, now time.Time, dur time.Duration) *Claims {
 	return &Claims{
 		Role: role,
 		RegisteredClaims: jwt.RegisteredClaims{
-			ID:        id,
-			Subject:   sub,
-			Issuer:    iss,
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24 * 30)),
+			Subject:   uuid,
+			IssuedAt:  jwt.NewNumericDate(now),
+			ExpiresAt: jwt.NewNumericDate(now.Add(dur)),
 		},
 	}
 }
 
-func (c *Claims) NewToken() string {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
-	signedToken, err := token.SignedString(secret)
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-
-	return signedToken
+func (c *Claims) Token() (string, error) {
+	t := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
+	return t.SignedString(secret)
 }
 
 func (c *Claims) Verify(token string) bool {
 	jwtToken, err := jwt.ParseWithClaims(token, c, func(t *jwt.Token) (interface{}, error) {
 		if _, ok := t.Method.(*jwt.SigningMethodHMAC); ok {
-			return []byte(secret), nil
-		}
-
-		exp, err := t.Claims.GetExpirationTime()
-		if err != nil {
-			return nil, jwt.ErrTokenInvalidClaims
-		}
-
-		if exp.After(time.Now()) {
-			return nil, jwt.ErrTokenExpired
+			return secret, nil
 		}
 
 		return nil, jwt.ErrInvalidKeyType
 	})
-
 	if err != nil {
-		fmt.Println(err)
 		return false
 	}
 
-	claims := jwtToken.Claims.(*Claims)
-	c.Role = claims.Role
-	c.RegisteredClaims = claims.RegisteredClaims
 	return jwtToken.Valid
+}
+
+func (c *Claims) Expired() bool {
+	return c.ExpiresAt.Time.Before(time.Now())
+}
+
+func (c *Claims) Store() error {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+	conn := rdb.Conn()
+	defer conn.Close()
+
+	ctx := context.Background()
+	token, err := c.Token()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = conn.Set(ctx, c.Subject, token, c.ExpiresAt.Sub(time.Now())).Err()
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- claims/claims_test.go
+++ claims/claims_test.go
@@ -2,27 +2,52 @@ package claims_test
 
 import (
 	"testing"
+	"time"
 
 	. "github.com/east-true/auth-go/jwt/claims"
+	"github.com/google/uuid"
 )
 
-func TestNewToken(t *testing.T) {
-	claims := New("auth_token", "test", "boardsvr", "admin")
-	token := claims.NewToken()
-	if token == "" {
+var claims *Claims
+
+func TestNew(t *testing.T) {
+	idgen, _ := uuid.NewRandom()
+	id := idgen.String()
+	now := time.Now()
+	dur := time.Duration(1 * time.Second)
+	claims = New(id, "test", now, dur)
+	if claims.Subject != id {
+		t.Fail()
+	}
+}
+
+func TestToken(t *testing.T) {
+	token, err := claims.Token()
+	if err != nil || token == "" {
 		t.Fail()
 	}
 }
 
 func TestVerify(t *testing.T) {
-	claims := New("auth_token", "test", "boardsvr", "admin")
-	token := claims.NewToken()
-	if token == "" {
+	token, err := claims.Token()
+	if err != nil || token == "" {
 		t.Fail()
 	}
 
-	newClaims := new(Claims)
-	if !newClaims.Verify(token) {
+	if !claims.Verify(token) {
+		t.Fail()
+	}
+}
+
+func TestNotExpired(t *testing.T) {
+	if claims.Expired() {
+		t.Fail()
+	}
+}
+
+func TestExpired(t *testing.T) {
+	time.Sleep(3 * time.Second)
+	if !claims.Expired() {
 		t.Fail()
 	}
 }
